90d7cdce3cd424bf4be411654fd71ebddda684d3
Praveen <78463167+PraveenTha@users.noreply.github.com>
2026-03-28 13:07:34 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { FaWhatsapp, FaEnvelope } from "react-icons/fa";
+import {  FaWhatsapp, FaEnvelope, FaPhone } from "react-icons/fa";
 import "../assets/css/footer.css";
 import SocialIcons from "./SocialIcons";
 
@@ -32,7 +32,7 @@ const Footer = () => {
   <a
     href="Hi%20%F0%9F%91%8B%0AThank%20you%20for%20contacting%20me!%0AI%E2%80%99ve%20received%20your%20message%20and%20will%20connect%20with%20you%20shortly.%0AHave%20a%20great%20day%20%F0%9F%98%8A%0A%E2%80%94%20Praveen"
     target="_blank"
-    rel="noopener noreferrer"
+    rel="noopener noreferrer."
     className="whatsapp"
     aria-label="WhatsApp"
   >
